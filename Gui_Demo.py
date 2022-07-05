7157e04a8fd62cb93c8f84f0d6a58b495a7b8864
daishuai <daishuai@kedacom.com>
2022-07-05 19:42:59 +0800

    自动评审代码

--- Gui_Demo.py
+++ Gui_Demo.py
@@ -1,4 +1,8 @@
+import os
+import sys
+import tkinter
 import time
+from tkinter import filedialog
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -6,8 +10,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
 
-def auto_code_review(username, password):
-    service = Service(r'chromedriver.exe')
+def auto_code_review(username, password, path):
+    service = Service(path)
     browser = webdriver.Chrome(service=service)
     browser.get('http://172.16.0.124/')
     browser.find_element(value='UQ0_0').send_keys(username)  # 输入用户名
@@ -44,3 +48,35 @@ def auto_code_review(username, password):
             li_elements = ul_elements[2].find_elements(by=By.CLASS_NAME, value='phui-oi-standard')
         cnt = len(li_elements)
     browser.close()
+
+
+def get_path(path_text):
+    path = filedialog.askopenfilename(title='请选择文件')
+    path_text.set(path)
+
+
+window = tkinter.Tk()
+window.title('代码评审')
+window.geometry('500x200')
+username_entry = tkinter.Entry(window, show='')
+username_label = tkinter.Label(window, text='输入用户名')
+username_entry.place(x=200, y=6)
+username_label.place(x=120, y=6)
+password_entry = tkinter.Entry(window, show='*')
+password_label = tkinter.Label(window, text='输入密码')
+password_entry.place(x=200, y=40)
+password_label.place(x=120, y=40)
+path_text = tkinter.StringVar()
+path_entry = tkinter.Entry(window, textvariable=path_text)
+path_label = tkinter.Label(window, text='请输入路径')
+path_entry.place(x=200, y=80)
+path_label.place(x=120, y=80)
+file_button = tkinter.Button(window, text='选择路径', command=lambda: get_path(path_text))
+exit_button = tkinter.Button(window, text='退出', command=lambda: window.destroy(), width=5, height=1)
+start_button = tkinter.Button(window, text='开始',
+                              command=lambda: auto_code_review(username_entry.get(), password_entry.get(), path_text.get()),
+                              width=5, height=1)
+file_button.place(x=350, y=75)
+exit_button.place(x=250, y=120, anchor='nw')
+start_button.place(x=190, y=120, anchor='nw')
+window.mainloop()
